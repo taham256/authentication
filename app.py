79d489b90d65b7a7e1de35478a9d9c93bc4a7b5e
taham256 <taham216@proton.me>
2025-10-13 23:38:51 +0530

    finally basic otp based login system done

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
 import smtplib
 import os
-import bcrypt
+import random
+import time
 
+otp_store = {}
 class Login_System:
     def __init__(self):
         self.official_email = os.getenv('OFFICIAL_EMAIL')
-        self.smtp_password = os.getenv('OFFICIAL_PASSWORD')
-
-    def send_email(self,reciver_email,subject,message):
+        self.smtp_password = os.getenv('SMTP_PASSWORD')
+        if not  self.official_email or not self.smtp_password:
+            raise ValueError("oops! missing email or password in environment variable")
+    def sign_in(self,user_name,user_email):
+        self.otp = random.randint(1000,9999)
+        otp_store[user_email] = {"otp":self.otp,
+                                 "time":time.time()}
         try:
-            server = smtplib.SMTP('smtp.gmail.com',587)
-            server.starttls()
-            server.login(self.official_email,self.smtp_password)
-            server.sendmail(self.official_email,reciver_email,
-                            f"Subject: {subject}\n\n{message}")
-            server.quit()
-            return True
+            with smtplib.SMTP('smtp.gmail.com', 587) as server:
+             server.starttls()
+             server.login(self.official_email, self.smtp_password)
+             server.sendmail(self.official_email, user_email,
+                            f"Subject:for login\n\nHI {user_name} your OTP is {self.otp}."
+                            f"Please keep it secret🤫.")
+             server.quit()
+             return True
         except Exception as e:
-            print(f"Error: {e}")
-            return False
+             print(f"Error: {e}")
+             return False
+    def enter_otp(self,user_email,given_otp):
+        otp_data = otp_store.get(user_email)
+        current_time = time.time()
+        expire_time = current_time - otp_data["time"]
+        if given_otp == otp_data["otp"] and expire_time < 300 :
+            print("Verified")
+        elif expire_time > 300:
+            print("OTP has expired")
+        else:
+            print("oops! wrong otp, try again")
 
-    def login(self,user_name,email,password):
-            pass
+
+
+ls = Login_System()
+ls.sign_in("taham","taham216@proton.me")
